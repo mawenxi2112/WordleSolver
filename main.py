fd4de4528809d5d907131de72489974ea7b91f60
mawenxi2112 <mawenxi2112@gmail.com>
2022-03-08 10:05:56 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,91 @@
+# need a list of possible 5 letter words DONE
+
+# while loop
+# 1. take input from user (word that they tried, the result BBBBG etc)
+# 2. check result, and process word
+# 3. print out words that can fall into to the result
+
+import time
+
+words = []
+current_words = []
+
+letter_options = {
+    "0": "⬜️",
+    "1": "🟩",
+    "2": "🟨"
+}
+
+with open('answer-list.txt') as f:
+    lines = f.readlines()
+
+    for line in lines:
+        words.append(line.strip().lower())
+
+    print("loaded possible words: " + str(len(words)) + "!\n")
+    current_words = words
+
+
+def possible_words(word_list, input_word, conditions):
+    start_time = time.time()
+
+    possible_word_list = []
+
+    for word in word_list:
+        invalid_letter_count = 0
+
+        for i, condition in enumerate(conditions):
+
+            if condition == '0':
+                invalid_letter_count = invalid_letter_count + 1
+
+                if input_word[i] in word:
+                    break
+
+            elif condition == '1':
+                if word[i] != input_word[i]:
+                    break
+
+            elif condition == '2':
+                if input_word[i] == word[i] or input_word[i] not in word:
+                    break
+
+            if invalid_letter_count == 5:
+                break
+
+            if i == len(conditions) - 1:
+                possible_word_list.append(word)
+
+    return possible_word_list, time.time() - start_time
+
+
+while True:
+    print("Example: adieu")
+    print("")
+    attempted_word = input("Enter word attempted: ").lower()
+    print("")
+
+    for item in letter_options:
+        print(letter_options[item] + " = " + item)
+    print("Example: 01220")
+    print("")
+
+    result_word = input("Enter word result:  ")
+    print("")
+
+    print(attempted_word)
+
+    for result in result_word:
+        print(letter_options[result], end=" ")
+
+    print("\n")
+
+    current_words, result_time = possible_words(current_words, attempted_word, result_word)
+
+    print(str(len(current_words)) + " possible results out of " + str(len(words)))
+    print("results took: " + str(result_time) + "s to return")
+
+    for word in current_words:
+        print(word)
+
+    print("-------------------------------------------------")
